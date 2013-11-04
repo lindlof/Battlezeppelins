b63fb1a11e2e91c1651c6190aeffdff130f78155
Mikael Lindlöf <mikuz.dev@gmail.com>
2013-11-04 22:17:29 +0200

    Add wip sql script and use the MySQL database.

--- Battlezeppelins/Controllers/HomeController.cs
+++ Battlezeppelins/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MySql.Data.MySqlClient;
 
 namespace Battlezeppelins.Controllers
 {
@@ -11,6 +13,29 @@ namespace Battlezeppelins.Controllers
         public ActionResult Index()
         {
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            MySqlConnection conn = new MySqlConnection(
+                ConfigurationManager.ConnectionStrings["BattlezConnection"].ConnectionString);
+            try
+            {
+                conn.Open();
+
+                string sql = "SELECT name FROM battlezeppelins.player WHERE id='1'";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    @ViewBag.playerName = rdr[0];
+                }
+                rdr.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            conn.Close();
+            Console.WriteLine("Done.");
 
             return View();
         }
---
+++ sql/battlezeppelins.sql
@@ -0,0 +1,29 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+CREATE SCHEMA IF NOT EXISTS `battlezeppelins` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `battlezeppelins` ;
+
+-- -----------------------------------------------------
+-- Table `battlezeppelins`.`Player`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `battlezeppelins`.`Player` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `battlezeppelins`.`Game`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `battlezeppelins`.`Game` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
