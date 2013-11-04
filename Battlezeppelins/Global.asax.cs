f799f1aaa719ae72b54a74aeb90dd6e6e6021503
Mikael Lindlöf <mikuz.dev@gmail.com>
2013-11-04 22:11:03 +0200

    Remove bogus authentication stuff. Add MySql references.

--- Battlezeppelins/Global.asax.cs
+++ Battlezeppelins/Global.asax.cs
@@ -22,7 +22,6 @@ namespace Battlezeppelins
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            AuthConfig.RegisterAuth();
         }
     }
 }
