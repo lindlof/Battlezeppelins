1e66935cdc50ef27fcdd26521e5c5b2a4ccf8aaa
Mikael Lindlöf <mikuz.dev@gmail.com>
2013-11-13 19:41:12 +0200

    Player can send a challenge request.

---
+++ Battlezeppelins/Controllers/RequestController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Battlezeppelins.Models;
+
+namespace Battlezeppelins.Controllers
+{
+    public class RequestController : Controller
+    {
+        public ActionResult BattleChallenge()
+        {
+            Player player = null;
+
+            if (Request.Cookies["userInfo"] != null)
+            {
+                string idStr = Server.HtmlEncode(Request.Cookies["userInfo"]["id"]);
+                int? id = Int32.Parse(idStr);
+                player = new Player(id);
+            }
+
+            string challengeeName = Request.Form["challengeeSelected"];
+
+            if (challengeeName != null)
+            {
+                
+            }
+
+            return null;
+        }
+    }
+}
