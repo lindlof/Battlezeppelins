9f1fcd32d66042f1ea59bdffd785c25f04d88766
Mikael Lindlöf <mikuz.dev@gmail.com>
2013-11-24 04:16:03 +0200

    Add game table objects and database entries.

---
+++ Battlezeppelins/Models/PlayerTable/GameTable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Battlezeppelins.Models
+{
+
+    public class GameTable
+    {
+        List<OpenPoint> openPoints;
+        List<Zeppelin> zeppelins;
+    }
+}
